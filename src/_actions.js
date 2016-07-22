e984b03deae338ba58c60aebc42f889d5fdb2085
Jianghai <marsandheart@gmail.com>
2016-07-22 18:51:21 +0800

    增加了一些兴趣点，围栏相关的action

--- src/_actions.js
+++ src/_actions.js
@@ -10,7 +10,20 @@ export const ACT ={
         GET_USERS:'GET_USERS',
         GET_CARS:'GET_CARS',
         GETED_USERS:'GETED_USERS',
-        GETED_CARS:'GETED_CARS'
+        GETED_CARS:'GETED_CARS',
+
+        GET_HOBBIES_START:'GET_HOBBIES_START',
+        GET_HOBBIES_END:'GET_HOBBIES_END',
+        ADD_HOBBY:'ADD_HOBBY',
+        DELETE_HOBBY:'DELETE_HOBBY',
+        UPDATE_HOBBY:'UPDATE_HOBBY',
+        SELECT_HOBBY:'SELECT_HOBBY',
+
+        GET_FENCES:'GET_FENCES',
+        ADD_FENCE:'ADD_FENCE',
+        DELETE_FENCE:'DELETE_FENCE',
+        UPDATE_FENCE:'UPDATE_FENCE',
+        SELECT_FENCE:'SELECT_FENCE'
     },
     'const':{
         all:'ALL'
@@ -62,11 +75,61 @@ export const ACT ={
         startGetCars:function () {
             return {type: ACT.action.GET_CARS};
         },
-        getedUsers:function (data) {
+        getedUsers:function (data) {//获取到所有用户数据
             return {type: ACT.action.GETED_USERS,data};
         },
-        getedCars:function (data) {
+        getedCars:function (data) {//获取到所有车辆数据
             return {type: ACT.action.GETED_CARS,data};
+        },
+
+        getHobbies:function(hobbies){//获取所有兴趣点
+            return function(dispatch){
+                dispatch(ACT.fun.getHobbiesStart());
+                W.get(AJAX[4].url,AJAX[4].data,function(res){
+                    dispatch(ACT.fun.getHobbiesEnd(res.data));
+                },AJAX[4].dataType);
+            }
+        },
+        getHobbiesStart:function(){//获取所有兴趣点，开始
+            return{type:ACT.action.GET_HOBBIES_START};
+        },
+        getHobbiesEnd:function(hobbies){//获取所有兴趣点，结束
+            return{type:ACT.action.GET_HOBBIES_END,hobbies};
+        },
+        addHobby:function(hobby){//添加一个兴趣点
+            return {type:ACT.action.ADD_HOBBY,hobby};
+        },
+        deleteHobby:function(hobby){//删除一个兴趣点
+            return {type:ACT.action.DELETE_HOBBY,hobby};
+        },
+        updateHobby:function(hobby){//更新一个兴趣点
+            return {type:ACT.action.UPDATE_HOBBY,hobby};
+        },
+        selectHobby:function(hobby){//设置当前已选择的兴趣点
+            return {type:ACT.action.SELECT_HOBBY,hobby};
+        },
+
+        getFences:function(fences){//获取所有围栏数据
+            return function(dispatch){
+                W.get(AJAX[5].url,AJAX[5].data,function(res){
+                    dispatch(ACT.fun.getFencesEnd(res.data));
+                },AJAX[5].dataType);
+            }
+        },
+        getFencesEnd(fences){//获取到所有围栏数据
+            return {type:ACT.action.GET_FENCES,fences};
+        },
+        addFence:function(fence){//添加一个围栏
+            return {type:ACT.action.ADD_FENCE,fence};
+        },
+        deleteFence:function(fence){//删除一个围栏
+            return {type:ACT.action.DELETE_FENCE,fence};
+        },
+        updateFence:function(fence){//更新一个围栏
+            return {type:ACT.action.UPDATE_FENCE,fence};
+        },
+        selectFence:function(fence){//设置当前已选择的围栏
+            return {type:ACT.action.SELECT_FENCE,fence};
         }
         
     }
@@ -121,5 +184,25 @@ const AJAX=[
             "t":1467702765610
         },
         "timeout":10000
+    },
+    {//兴趣点
+        "url":"http://web.wisegps.cn/app/customer/237/poi",
+        "type":"GET",
+        "dataType":"json",
+        "data":{
+            "auth_code":code,
+            "is_geo":0,
+            "rand":0.9083654713010103
+        }
+    },
+    {//围栏
+        "url":"http://web.wisegps.cn/app/customer/237/poi",
+        "type":"GET",
+        "dataType":"json",
+        "data":{
+            "auth_code":code,
+            "is_geo":1,
+            "rand":0.15131601312031773
+        }
     }
 ]
